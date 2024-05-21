78dbff515356c50cc813671f35d82fbcfe0b2b59
Rinat <bastanov.rinaj@yandex.ru>
2024-05-21 16:30:52 +0300

    Added book id

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../../redux/books/actionCreators';
 import './BookForm.css';
 
@@ -16,6 +17,7 @@ const BookForm = () => {
       const book = {
         title,
         author,
+        id: uuidv4(),
       };
       dispatch(addBook(book));
       setTitle('');
