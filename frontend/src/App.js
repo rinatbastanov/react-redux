b8ee0179dff35562f9c7ea978843794df6db5ed2
Rinat <bastanov.rinaj@yandex.ru>
2024-05-21 11:52:06 +0300

    Initial React App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="app-header">
+        <h1>Book Librery app</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
